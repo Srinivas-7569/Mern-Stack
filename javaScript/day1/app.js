2a6adc8a464114bc7d6bb80820a6413c3ad09d56
Nallagonda Srinivas <nallagondasrinivas30@gmail.com>
2025-06-16 14:16:34 +0530

    day1

--- javaScript/day1/app.js
+++ javaScript/day1/app.js
@@ -1,1 +1,18 @@
 console.log("hello");
+//functions in java script
+// let juice=50;
+// let gstonjuice=0.1;
+// let fries=70;
+// let gstonfood=0.05;
+// let juiceBillAmount=juice+juice*gstonjuice;
+// let friesBillAmount=const finalBill=juiceBillAmount+friesBillAmount;
+// fries+fries*gstonjuice;
+// console.log("final bill",finalBill)
+function calculateJuiceBillAmount(){
+    let juice=50;
+    let gstonjuice=0.1;
+    let juuiceBillAmount=juice+juice*gstonjuice;
+    console.log(juuiceBillAmount);
+}
+calculateJuiceBillAmount();
+console.log("END");
