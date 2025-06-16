542c80ca6986f510f3463d12f8a0f6061b0f5f21
Nallagonda Srinivas <nallagondasrinivas30@gmail.com>
2025-06-16 15:00:36 +0530

    day1

--- javaScript/day1/app.js
+++ javaScript/day1/app.js
@@ -1,4 +1,4 @@
-console.log("hello");
+
 //functions in java script
 // let juice=50;
 // let gstonjuice=0.1;
@@ -8,11 +8,55 @@ console.log("hello");
 // let friesBillAmount=const finalBill=juiceBillAmount+friesBillAmount;
 // fries+fries*gstonjuice;
 // console.log("final bill",finalBill)
-function calculateJuiceBillAmount(){
-    let juice=50;
-    let gstonjuice=0.1;
-    let juuiceBillAmount=juice+juice*gstonjuice;
-    console.log(juuiceBillAmount);
-}
-calculateJuiceBillAmount();
-console.log("END");
+// let age =21;
+// console.log("start",age);
+// function calculateJuiceBillAmount(){
+//     let juice=50;
+//     let gstonjuice=0.1;
+//     let juuiceBillAmount=juice+juice*gstonjuice;
+//     console.log(juuiceBillAmount);
+// }
+// calculateJuiceBillAmount();
+// console.log("END");
+// --------------------------------------------------------------------
+// function calculateJuiceBillAmount(juice){
+//     let gstonjuice=0.1;
+//     let juuiceBillAmount=juice+juice*gstonjuice;
+//     console.log(juuiceBillAmount);
+// }
+// calculateJuiceBillAmount(60);
+// calculateJuiceBillAmount(50);
+// calculateJuiceBillAmount(110);
+// ----------------------------------------------------------------------
+// function printBill(name,amount){
+//     console.log("------------------");
+//     console.log("your bill for",name);
+//     console.log("rs.",amount);
+//     console.log("----------------------");
+
+// }
+// function  calculateBillAmount(price,gst){
+//     console.log("....calculating...");
+//     let billAmount=price+price*gst;
+//     return billAmount;
+// }
+// let res1=calculateBillAmount(60,0.1);
+// printBill("mango",res1);
+// let res2=calculateBillAmount(70,0.2);
+// printBill("orange",res2);
+// let res3=calculateBillAmount(50,0.3);
+// printBill("banana",res3);
+// -----------------------------------------------------------------------------
+//function assignment()
+const printbill=function printBill(name,amount){
+     console.log("------------------");
+    console.log("your bill for",name);
+    console.log("rs.",amount);
+    console.log("----------------------");
+};
+//function assignment(anonymous function)
+const calculateBillAmount=function  calculateBillAmount(price,gst){
+console.log("....calculating...");
+  let billAmount=price+price*gst;
+  return billAmount;
+};
